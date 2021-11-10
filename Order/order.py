8d4fbb241630bc422142845cf465e96ecef87fc4
ShazaYousef <93592258+ShazaYousef@users.noreply.github.com>
2021-11-10 21:43:11 +0200

    Create order.py
    the implementation of the order server, it well first make sure of the book id and its quantity before make update request to the catalog server.

---
+++ Order/order.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+from flask import request
+import json
+import requests
+
+def foo():
+    return request.json['inputVar']
+
+app = Flask(__name__)
+CORS(app)
+CORS(app, support_credentials=True)
+
+@app.route("/login")
+@cross_origin(supports_credentials=True)
+
+def login():
+  return jsonify({'success': 'ok'})
+    
+
+@app.route('/purchase/<id>', methods=['PUT'])
+def search_api(id):  
+    r = requests.get('http://127.0.0.1:5002/query/info/'+id)
+    js = r.json()
+
+    if(len(js)==0):
+        return {"status":"fail, NO such book"}
+
+    q = js[0]["quantity"]
+    if(int(q) > 0):
+        rs = requests.put('http://127.0.0.1:5002/update/'+id)
+        return rs.text
+
+    return {"status":"fail, all soled"}
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003, host="127.0.0.1") 
+
+
