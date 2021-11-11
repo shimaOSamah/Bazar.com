02f7b869eda65eb73afea6e7bbe6d193c78a4bef
shima osamah <60664444+shimaOSamah@users.noreply.github.com>
2021-11-11 14:16:48 +0200

    Update main.py
    update IP address for query and purchase services.
    update Host IP

--- Front_APIs/main.py
+++ Front_APIs/main.py
@@ -20,27 +20,27 @@ def login():
 
 @app.route('/search/<cat>')
 def search_api(cat):  
-    r = requests.get("http://127.0.0.1:5002/query/search/"+cat)
+    r = requests.get("http://192.168.187.130:5000/query/search/"+cat)
     return r.text
 
 @app.route('/search')
 def search():
-    r = requests.get("http://127.0.0.1:5002/query/search")
+    r = requests.get("http://192.168.187.130:5000/query/search")
     return r.text
 
 @app.route('/info/<id>')
 def info_api(id):  
-    r = requests.get("http://127.0.0.1:5002/query/info/"+id)
+    r = requests.get("http://192.168.187.130:5000/query/info/"+id)
     return r.text
 
 @app.route('/info')
 def info():  
-    r = requests.get("http://127.0.0.1:5002/query/info")
+    r = requests.get("http://192.168.187.130:5000/query/info")
     return r.text
 
 @app.route('/purchase/<id>', methods=["PUT"])
 def purchase_api(id): 
-    r = requests.put("http://127.0.0.1:5003/purchase/"+id) 
+    r = requests.put("http://192.168.187.129:5000/purchase/"+id) 
     js = r.json()
     return js["status"]
 
@@ -50,4 +50,4 @@ def purchase():
 
 
 if __name__ == "__main__":
-    app.run(debug=True ) 
+    app.run(debug=True, host="192.168.187.128") 
